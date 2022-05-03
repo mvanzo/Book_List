b1f5cf434a496b235da9c629e40a37854af74ea8
mvanzo <97002794+mvanzo@users.noreply.github.com>
2022-05-03 12:18:27 -0700

    updates alert

--- app.js
+++ app.js
@@ -50,6 +50,18 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div')
+        div.className = `alert alert-${className}`
+        div.appendChild(document.createTextNode(message))
+
+        const container = document.querySelector('.container')
+        const form = document.querySelector('#book-form')
+        
+        container.insertBefore(div, form)
+
+    }
+
     static clearFields() {
         document.getElementById('title').value = ''
         document.getElementById('author').value = ''
